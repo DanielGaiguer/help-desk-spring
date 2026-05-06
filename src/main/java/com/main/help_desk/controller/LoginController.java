ba037231ef570d4239a979ca21de52d4ff057c47
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-05-06 17:29:30 -0300

    create token JJWT

---
+++ src/main/java/com/main/help_desk/controller/LoginController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.main.help_desk.controller;
+
+import com.main.help_desk.model.UsuarioDTO;
+import com.main.help_desk.service.TokenService;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api")
+public class LoginController {
+    @Autowired
+    private TokenService service;
+    
+    @GetMapping("/token")
+    public String getToken(@RequestBody UsuarioDTO usuario){
+        return service.getToken(usuario);
+    }
+    
+    @PostMapping("/validar-token")
+    public String validarToken(@RequestParam String token) {
+        if (service.validToken(token)) {
+            Claims claims = service.extrairClaims(token);
+            return "Token válido! Subject: " + claims.getSubject() + 
+                   ", Emitido em: " + claims.getIssuedAt() + 
+                   ", Expira em: " + claims.getExpiration();
+        } else {
+            return "Token inválido ou expirado!";
+        }
+    }
+}
---
+++ src/main/java/com/main/help_desk/service/TokenService.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.main.help_desk.service;
+import com.main.help_desk.model.UsuarioDTO;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    
+    private SecretKey getSignKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(this.secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    
+    public String getToken(UsuarioDTO usuario) {
+        return Jwts.builder()
+            .subject(usuario.getEmail())
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + 300000))
+            .signWith(getSignKey())
+            .compact();
+    }
+    
+    public boolean validToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(getSignKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+    
+    public Claims extrairClaims(String token) {
+        // Analisa o token com a chave secreta
+        return Jwts.parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                // Retorna o corpo do token (claims)
+                .getBody();
+    }
+}
