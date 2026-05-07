02003e32ac5f252153f3a4d12ca1d726022cfe0f
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-05-07 13:42:35 -0300

    feat: test authorization AUTH

--- src/main/java/com/main/help_desk/controller/LoginController.java
+++ src/main/java/com/main/help_desk/controller/LoginController.java
@@ -11,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api")
+@RestController
+@RequestMapping("api")
 public class LoginController {
     @Autowired
     private TokenService service;
